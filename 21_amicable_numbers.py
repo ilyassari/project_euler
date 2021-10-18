94bd49905f13d12475a99e3e662483ef55229fef
ilyassari <ilyassari@yahoo.com>
2021-10-18 19:51:20 +0300

    initial

---
+++ 21_amicable_numbers.py
@@ -0,0 +1,43 @@
+'''
+Problem 21 Amicable numbers
+
+Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+Evaluate the sum of all the amicable numbers under 10000.
+'''
+
+def all_factors(number):
+    """ take a number; give numbers all factors"""
+    divider = 2
+    factors = [1]
+    while divider <= number / 2:
+        if number % divider == 0:
+            factors.append(divider)
+        divider += 1
+    return factors
+
+def d(n):
+    """take a number; give sum of all factors"""
+    return sum(all_factors(n))
+
+def check_amicable_numbers(number):
+    """ take a number, give its an amicable number or not """
+    return number == d(d(number)) and number != d(number)
+
+def amicable_numbers(boundary):
+    """take a number as upper limit; give amicable numbers as list"""
+    nums = list()
+    for i in range(1, boundary):
+        if check_amicable_numbers(i):
+            nums.append(i)
+    return nums
+
+def sum_of_amicable_numbers(boundary):
+    """take a number as upper limit; give sum of amicable numbers"""
+    return sum(amicable_numbers(boundary))
+
+
+print(sum_of_amicable_numbers(10000)) #31626
